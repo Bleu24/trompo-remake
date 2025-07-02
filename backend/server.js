457e32abd26c0670312ecf32003cc26abc94d877
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-07-02 15:50:14 +0800

    initialize server with Express, CORS, and database connection

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.config');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/owner', require('./routes/businessOwner.routes'));
+app.use('/api/businesses', require('./routes/business.routes'));
+
+
+
+// Start server
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
