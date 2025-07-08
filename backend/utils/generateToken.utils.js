715703386f6d39ee3739926e5b3fb08a91c28623
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-07-09 01:38:19 +0800

    Add user name and email to JWT token generation

--- backend/utils/generateToken.utils.js
+++ backend/utils/generateToken.utils.js
@@ -2,7 +2,12 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (user) => {
   return jwt.sign(
-    { userId: user._id, role: user.role },
+    { 
+      userId: user._id, 
+      role: user.role,
+      name: user.name,
+      email: user.email
+    },
     process.env.JWT_SECRET,
     { expiresIn: '7d' }
   );
