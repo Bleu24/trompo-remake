208961e038845a11709da2250f6e70e9f92b2904
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-07-08 22:42:49 +0800

    Wrap layout components with ThemeProvider for improved theming support

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/ui/navbar";
 import ChatWidget from "@/app/chat/ChatWidget";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
-        <ChatWidget />
+        <ThemeProvider>
+          <Navbar />
+          {children}
+          <ChatWidget />
+        </ThemeProvider>
       </body>
     </html>
   );
