92be81bf4b6963da51504642bfe7ee7f1f5e302e
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-07-12 01:58:25 +0800

    Add profilePicture field to user schema for enhanced user profile management

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: { type: String, enum: ['admin', 'customer', 'owner'], required: true },
   image: String,
+  profilePicture: String,
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
